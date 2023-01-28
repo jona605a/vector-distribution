fabef5e22b277090e879b6bb070f918619464843
jona605a <jonathan.hoejlev@gmail.com>
2023-01-28 22:06:41 +0100

    Fixed abs

--- data.jl
+++ data.jl
@@ -25,7 +25,7 @@ end
 Vbyggetek = ReadHiredVectors("Data/Primary Distribution.xlsx - C. Byggeteknologi.csv")
 Vbygdes = ReadHiredVectors("Data/Primary Distribution.xlsx - C. Bygningsdesing.csv")
 
-# show(names(Vbygdes)) # gives the following:
+# show(names(AllVectors)) # gives the following:
 # ["Want to hire", "Name", "Has been vector before", "Wants Small Trip", "Wants Medium Tri", "Wants Large Trip", 
 # ">20 og kørekort i min. 1 år", "Ok with English Trips", "Prefer English Trips", "Ok with Sober English Trips", 
 # "Prefer Sober English Trips", "Ok with 1-Day Trips", "Prefer 1-Day Trips", "Ok with Weekend Trips", "Prefer Weekend Trips", 
@@ -33,12 +33,22 @@ Vbygdes = ReadHiredVectors("Data/Primary Distribution.xlsx - C. Bygningsdesing.c
 # "Sex", "Access to Sowing Machine", "Power level", "Study line team", "Lyngby/Ballerup"]
 
 AllVectors = vcat(Vbyggetek,Vbygdes)
+AllVectors = transform(AllVectors, :"Sex" => ByRow(x -> x=="M") => :"Male")
+AllVectors = transform(AllVectors, :"Lyngby/Ballerup" => ByRow(x -> x=="B") => :"Ballerup")
+
+# AllVectors = transform(AllVectors, :"Study line team" => ByRow(x -> x) => :"ForbiddenTrips")
+
+# collect(eachsplit(StudylineKABS[findfirst(y->y=="D. Produktion", StudylineKABS[:,1]) , 2],","))
+
+
+# show(names(AllVectors)) # gives the following:
 
 V = size(AllVectors, 1)
 
 Femaleratio = size(subset(AllVectors, :"Sex" => a-> isequal.(a,"F")))[1] / V
 AvgSndTime = size(subset(AllVectors, :"Has been vector before" => a->a))[1] / V
 AvgDrivers = size(subset(AllVectors, :">20 og kørekort i min. 1 år" => a->a))[1] / V
+AvgSmokers = size(subset(AllVectors, :"Smoker" => a->a))[1] / V
 
 
 
--- vector-dist.jl
+++ vector-dist.jl
@@ -11,7 +11,9 @@ println("Defining variables...")
 include("data.jl")
 
 if sum(VectorsPerTrip) != V
-    println("Error, the number of vectors $(V) and required sum of vectors $(sum(VectorsPerTrip)) do not match")
+    println("\n###################################################
+             \nError, the number of vectors ($(V)) and required sum of vectors ($(sum(VectorsPerTrip))) do not match
+             \n###################################################\n")
 end
 
 
@@ -30,12 +32,15 @@ println("Defining model...")
 @variable(Prob, NoCampus[1:N]>=0)
 @variable(Prob, AbsSndTime[1:N]>=0)
 @variable(Prob, AbsDrivers[1:N]>=0)
+@variable(Prob, AbsSmokers[1:N]>=0)
 
 # Helper variables
 @variable(Prob, MoreSndTime[1:N]>=0)
 @variable(Prob, LessSndTime[1:N]>=0)
 @variable(Prob, MoreDrivers[1:N]>=0)
 @variable(Prob, LessDrivers[1:N]>=0)
+@variable(Prob, MoreSmokers[1:N]>=0)
+@variable(Prob, LessSmokers[1:N]>=0)
 @variable(Prob, HasBallerupVector[1:N], Bin)
 
 @objective(Prob,Max,
@@ -45,16 +50,17 @@ println("Defining model...")
     - sum(NoCampus[n] for n=1:N)
     - sum(AbsSndTime[n] for n=1:N)
     - sum(AbsDrivers[n] for n=1:N)
+    - sum(AbsSmokers[n] for n=1:N)
 )
 
-@constraint(Prob, [n=1:N], sum(Assign[v,n] for v=1:V) == VectorsPerTrip[n]) # Each trip gets the vectors it needs
+@constraint(Prob, [n=1:N], sum(Assign[v,n] for v=1:V) <= VectorsPerTrip[n]) # Each trip gets the vectors it needs
 @constraint(Prob, [v=1:V], sum(Assign[v,n] for n=1:N) == 1)                 # Each vector is assigned once
 
 @constraint(Prob, [n=1:N,v=1:V,v2=1:V], SameTeam[v,v2] <= Assign[v,n] - Assign[v2,n] + 1) # Define SameTeam
 @constraint(Prob, [n=1:N,v=1:V,v2=1:V], SameTeam[v,v2] >= Assign[v,n] - Assign[v2,n] - 1) # Define SameTeam
 
-@constraint(Prob, [n=1:N], sum(Assign[v,n]*(AllVectors[v,"Sex"]=="M") for v=1:V) <= VectorsPerTrip[n]*(1-Femaleratio) + 1 + MoreMales[n])   # Define MoreMales
-@constraint(Prob, [n=1:N], sum(Assign[v,n]*(AllVectors[v,"Sex"]=="F") for v=1:V) <= VectorsPerTrip[n]*Femaleratio     + 1 + MoreFemales[n]) # Define MoreFemales
+@constraint(Prob, [n=1:N], sum(Assign[v,n]*AllVectors[v,"Male"]     for v=1:V) <= VectorsPerTrip[n]*(1-Femaleratio) + 1 + MoreMales[n])   # Define MoreMales
+@constraint(Prob, [n=1:N], sum(Assign[v,n]*(1-AllVectors[v,"Male"]) for v=1:V) <= VectorsPerTrip[n]*Femaleratio     + 1 + MoreFemales[n]) # Define MoreFemales
 
 @constraint(Prob, [n=1:N], sum(Assign[v,n]*AllVectors[v,"Access to Sowing Machine"] for v=1:V) >= 2 - FewerSew[n]) # Define FewerSew
 @constraint(Prob, [n=1:N], sum(Assign[v,n]*AllVectors[v,"Lives on Campus"] for v=1:V) >= 1 - NoCampus[n])          # Define NoCampus
@@ -62,15 +68,21 @@ println("Defining model...")
 # Distribute evenly the amount of 2nd time vectors
 @constraint(Prob, [n=1:N],   sum(Assign[v,n]*AllVectors[v,"Has been vector before"] for v=1:V) - AvgSndTime  <= MoreSndTime[n])
 @constraint(Prob, [n=1:N], -(sum(Assign[v,n]*AllVectors[v,"Has been vector before"] for v=1:V) - AvgSndTime) <= LessSndTime[n])
-@constraint(Prob, [n=1:N], AbsSndTime[n] >= MoreSndTime[n])
-@constraint(Prob, [n=1:N], AbsSndTime[n] >= LessSndTime[n])
+@constraint(Prob, [n=1:N], AbsSndTime[n] >= MoreSndTime[n] + LessSndTime[n])
 
 # Distribute evenly the amount of drivers
 @constraint(Prob, [n=1:N],   sum(Assign[v,n]*AllVectors[v,">20 og kørekort i min. 1 år"] for v=1:V) - AvgDrivers  <= MoreDrivers[n])
 @constraint(Prob, [n=1:N], -(sum(Assign[v,n]*AllVectors[v,">20 og kørekort i min. 1 år"] for v=1:V) - AvgDrivers) <= LessDrivers[n])
-@constraint(Prob, [n=1:N], AbsDrivers[n] >= MoreDrivers[n])
-@constraint(Prob, [n=1:N], AbsDrivers[n] >= LessDrivers[n])
+@constraint(Prob, [n=1:N], AbsDrivers[n] >= MoreDrivers[n] + LessDrivers[n])
 
+# Distribute evenly the amount of smokers
+@constraint(Prob, [n=1:N],   sum(Assign[v,n]*AllVectors[v,"Smoker"] for v=1:V) - AvgSmokers  <= MoreSmokers[n])
+@constraint(Prob, [n=1:N], -(sum(Assign[v,n]*AllVectors[v,"Smoker"] for v=1:V) - AvgSmokers) <= LessSmokers[n])
+@constraint(Prob, [n=1:N], AbsSmokers[n] >= MoreSmokers[n] + LessSmokers[n])
+
+# If there are any Ballerup vectors, there has to be at least 2
+@constraint(Prob, [n=1:N], sum(Assign[v,n]*AllVectors[v,"Ballerup"] for v=1:V) <= HasBallerupVector[n] * VectorsPerTrip[n])
+@constraint(Prob, [n=1:N], sum(Assign[v,n]*AllVectors[v,"Ballerup"] for v=1:V) >= HasBallerupVector[n] * 2)
 
 # show(names(Vbygdes)) # gives the following:
 # ["Want to hire", "Name", "Has been vector before", "Wants Small Trip", "Wants Medium Tri", "Wants Large Trip", 
@@ -79,11 +91,14 @@ println("Defining model...")
 # "Ok with Sober Weekend Trip", "Prefer Sober Weekend Trip", "Ok with Campus Trip", "Prefer Campus Trip", "Smoker", "Lives on Campus", 
 # "Sex", "Access to Sowing Machine", "Power level", "Study line team", "Lyngby/Ballerup"]
 
-# If there are any Ballerup vectors, there has to be at least 2
-@constraint(Prob, [n=1:N], sum(Assign[v,n]*(AllVectors[v,"Lyngby/Ballerup"]=="B") for v=1:V) <= HasBallerupVector[n] * VectorsPerTrip[n])
-@constraint(Prob, [n=1:N], sum(Assign[v,n]*(AllVectors[v,"Lyngby/Ballerup"]=="B") for v=1:V) >= HasBallerupVector[n] * 2)
-
-
+# ["Wants Small Trip", "Wants Medium Tri", "Wants Large Trip", 
+# "Ok with English Trips", "Prefer English Trips", 
+# "Ok with Sober English Trips", "Prefer Sober English Trips", 
+# "Ok with 1-Day Trips", "Prefer 1-Day Trips", 
+# "Ok with Weekend Trips", "Prefer Weekend Trips", 
+# "Ok with Sober Weekend Trip", "Prefer Sober Weekend Trip", 
+# "Ok with Campus Trip", "Prefer Campus Trip", 
+# "Power level", "Study line team"]
 
 
 
@@ -98,7 +113,6 @@ println("Termination status: $(termination_status(Prob))")
 #************************************************************************
 if termination_status(Prob) == MOI.OPTIMAL
     println("\nOptimal objective value: $(objective_value(Prob))")
-    
 else
     println("No optimal solution available")
 end
